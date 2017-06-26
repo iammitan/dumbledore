86c7872650fb2afc1ddfb90aeafb933a179c125a
Raghavendra Prabhu <iammitan@gmail.com>
2017-06-26 15:34:47 +0530

    Updating the files

---
+++ Test Project/src/com/iammitan/dumbledore/CassandraConnector.java
@@ -0,0 +1,10 @@
+package com.iammitan.dumbledore;
+
+public class CassandraConnector {
+	
+	public void PrintTest ()
+	{
+		System.out.println("Check Cassandra Connector");
+	}
+
+}
---
+++ Test Project/src/com/iammitan/dumbledore/Main.java
@@ -0,0 +1,14 @@
+package com.iammitan.dumbledore;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+		CassandraConnector connect = new CassandraConnector() ;
+		connect.PrintTest(); 
+	}
+
+}
+
+
